99c2abb43626fb3211b92512d6ef38a5385f2342
Jessica Chen <jessica.scoltock@protonmail.com>
2022-07-05 03:55:30 +0100

    Handle shiny pokemon

--- rownown.user.js
+++ rownown.user.js
@@ -8,14 +8,33 @@
 // @grant        none
 // ==/UserScript==
 
-const normal = PokedexHelper.getImage.bind(PokedexHelper)
-const better = (id) => {
-    if (Math.floor(Number(id)) === 201) {
-        const name = `R${String(id).split('.').map(Number).join('-')}`
-        return `https://aegyo.github.io/Rownown/images//${name}.png`
+const dexNormal = PokedexHelper.getImage.bind(PokedexHelper);
+const helpNormal = PokemonHelper.getImage.bind(PokemonHelper);
+
+const dexShowShinies = () => PokedexHelper.toggleAllShiny ? PokedexHelper.toggleAllShiny() : !PokedexHelper.hideShinyImages()
+
+const isUnown = (id) => Math.floor(Number(id)) === 201;
+const unown = (id, shiny) => {
+    const name = `R${String(id).split('.').map(Number).join('-')}${shiny ? '-s' : ''}`
+    return `https://aegyo.github.io/Rownown/images//${name}.png`
+}
+
+const betterDex = (id) => {
+    if (isUnown(id)) {
+        const shiny = App.game.party.alreadyCaughtPokemon(id, true) && dexShowShinies();
+        return unown(id, shiny);
     } else {
-        return normal(id)
+        return dexNormal(id)
     }
 }
-PokedexHelper.getImage = better;
-PokemonHelper.getImage = (p) => better(p.id);
+
+const betterHelp = (p, shiny) => {
+    if (isUnown(p.id)) {
+        return unown(p.id, shiny);
+    } else {
+        return helpNormal(p, shiny)
+    }
+}
+
+PokedexHelper.getImage = betterDex;
+PokemonHelper.getImage = betterHelp;
